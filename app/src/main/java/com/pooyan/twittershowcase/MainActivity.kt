38641e2425dfcf7d2d8da78d184f097f527a2dc3
pooyan <pooyanmajdabadi@candyspace.com>
2023-03-01 14:22:52 +0000

    Initial commit

---
+++ app/src/main/java/com/pooyan/twittershowcase/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.pooyan.twittershowcase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pooyan.twittershowcase.ui.theme.TwitterShowCaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TwitterShowCaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TwitterShowCaseTheme {
+        Greeting("Android")
+    }
+}
